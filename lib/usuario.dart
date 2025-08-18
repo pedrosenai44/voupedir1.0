4c1e2944644c4a5875d67ef4fd5af896b122e416
pedrosenai44 <pedrohenriquephnf884@gmail.com>
2025-08-18 11:52:17 -0300

    projeto de voupedir criando classes de usuario

--- lib/usuario.dart
+++ lib/usuario.dart
@@ -1,13 +1,32 @@
 class Usuario{
-  int? codigo;
-  String? nome;
-  String? login;
-  String? senha;
+  int? _codigo;
+  String? _nome;
+  String? _login;
+  String? _senha;
 
-  Usuario({
-    this.codigo,
-    this.senha,
-    this.login,
-    this.nome,
-});
+  Usuario({int? codigo, String? senha, String? login, String? nome
+}){
+    _codigo = codigo;
+    _login = login;
+    _nome = nome;
+    _senha = senha;
+  }
+  //geters
+  //exemlp tradicional
+String? get login{
+    return _login;
+}
+//metodo ao estilo dart
+int? get codigo => _codigo;
+  String? get nome => _nome;
+  String? get senha => _senha;
+//setters
+//setter tradicional
+set login (String? login){
+  _login = login;
+}
+//metedo set no estilo dart
+set nome (String? nome) => _nome = nome;
+set senha (String? senha) => _senha = senha;
+set codigo (int? codigo) => _codigo = codigo;
 }
